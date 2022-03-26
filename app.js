678e97c635b5cf2086ff9b8e4112e9fb7280b452
Kanta <azumakanta182@gmail.com>
2022-03-26 21:59:50 +0900

    Enable to update posts and make flash message available from every views

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.use(sessionOptions)
 app.use(flash())
 
 app.use(function(req, res, next) {
+  // make error and success flash messages available from all templeates
+  res.locals.errors = req.flash("errors")
+  res.locals.success = req.flash("success")
+
   // make current user id available on the req oblect
   if (req.session.user) {req.visitorId = req.session.user._id} else {req.visitorId = 0}
 
