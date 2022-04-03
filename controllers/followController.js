9d04ab7c555575ce13ea58f7a53f812ebe3be4d1
Kanta <azumakanta182@gmail.com>
2022-04-03 11:44:43 +0900

    Enable users to stop following

--- controllers/followController.js
+++ controllers/followController.js
@@ -3,12 +3,25 @@ const Follow = require('../models/Follow')
 exports.addFollow = function(req, res) {
   let follow = new Follow(req.params.username, req.visitorId)
   follow.create().then(() => {
-    req.flash("success", `Successfully follwed ${req.params.username}`)
+    req.flash("success", `Successfully followed ${req.params.username}`)
     req.session.save(() => res.redirect(`/profile/${req.params.username}`))
   }).catch((errors) => {
     errors.forEach(error => {
       req.flash("errors", error)
-      req.session.save(() => res.redirect('/'))
     })
+    req.session.save(() => res.redirect('/'))
+  })
+}
+
+exports.removeFollow = function(req, res) {
+  let follow = new Follow(req.params.username, req.visitorId)
+  follow.delete().then(() => {
+    req.flash("success", `Successfully stopped following ${req.params.username}`)
+    req.session.save(() => res.redirect(`/profile/${req.params.username}`))
+  }).catch((errors) => {
+    errors.forEach(error => {
+      req.flash("errors", error)
+    })
+    req.session.save(() => res.redirect('/'))
   })
 }
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,25 @@
 const User = require('../models/User')
 const Post = require('../models/Post')
+const Follow = require('../models/Follow')
+
+exports.sharedProfileData = async function(req, res, next) {
+  let isVisitorsProfile = false
+  let isFollowing = false
+  if (req.session.user) {
+    isVisitorsProfile = req.profileUser._id.equals(req.session.user._id)
+    isFollowing = await Follow.isVisitorFollowing(req.profileUser._id, req.visitorId)
+  }
+
+  req.isVisitorsProfile = isVisitorsProfile
+  req.isFollowing = isFollowing
+  next()
+}
 
 exports.mustBeLoggedIn = function(req, res, next) {
   if (req.session.user) {
     next()
   } else {
-    req.flash("errors", "You must be logged in to perform that action")
+    req.flash("errors", "You must be logged in to perform that action.")
     req.session.save(function() {
       res.redirect('/')
     })
@@ -54,7 +68,7 @@ exports.home = function(req, res) {
   if (req.session.user) {
     res.render('home-dashboard')
   } else {
-    res.render('home-guest', { regErrors: req.flash('regErrors')})
+    res.render('home-guest', {regErrors: req.flash('regErrors')})
   }
 }
 
@@ -66,17 +80,20 @@ exports.ifUserExists = function(req, res, next) {
     res.render("404")
   })
 }
+
 exports.profilePostsScreen = function(req, res) {
-  // ask our post model for posts by a creation authoer id
+  // ask our post model for posts by a certain author id
   Post.findByAuthorId(req.profileUser._id).then(function(posts) {
+    console.log(req.profileUser)
     res.render('profile', {
       posts: posts,
       profileUsername: req.profileUser.username,
-      profileAvatar: req.profileUser.avatar
+      profileAvatar: req.profileUser.avatar,
+      isFollowing: req.isFollowing,
+      isVisitorsProfile: req.isVisitorsProfile
     })
   }).catch(function() {
     res.render("404")
   })
-
 
 }
